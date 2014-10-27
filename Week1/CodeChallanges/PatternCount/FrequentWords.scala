98d2614cdb536cdf58de7da6af5056eede8b4852
John Devivo <jdevivo@gmail.com>
2014-10-27 13:25:51 -0400

    FrequentWords now returns a tuple containing the number of occurances and a list of the most frequent string

--- Week1/CodeChallanges/PatternCount/FrequentWords.scala
+++ Week1/CodeChallanges/PatternCount/FrequentWords.scala
@@ -2,28 +2,26 @@ import scala.collection.mutable.Map
 
 object FrequentWords {
 
-	def main(args: Array[String]):Unit = {
-		val result = dirtyFreqWords("CAGTTTCTAGATGGGCCCCGAGGGAGATGGGCCTGGTCCTTACCGAGGGCCGAGGGTGGTCCTTACAGTTTCTAGATGGGCCAGATGGGCCCCGAGGGGCGTCGCAAGATGGGCCCCGAGGGTGGTCCTTAAGATGGGCCGCGTCGCATGGTCCTTATGGTCCTTACAGTTTCTTGGTCCTTACAGTTTCTGCGTCGCAAGATGGGCCGCGTCGCACAGTTTCTCAGTTTCTCAGTTTCTCCGAGGGGCGTCGCAAGATGGGCCAGATGGGCCAGATGGGCCAGATGGGCCAGATGGGCCCCGAGGGCAGTTTCTAGATGGGCCTGGTCCTTAAGATGGGCCCAGTTTCTAGATGGGCCGCGTCGCATGGTCCTTATGGTCCTTACAGTTTCTAGATGGGCCCCGAGGGCAGTTTCTTGGTCCTTATGGTCCTTATGGTCCTTATGGTCCTTAAGATGGGCCCCGAGGGGCGTCGCAGCGTCGCATGGTCCTTATGGTCCTTAGCGTCGCAAGATGGGCCCCGAGGGTGGTCCTTAGCGTCGCAGCGTCGCAGCGTCGCATGGTCCTTAGCGTCGCACCGAGGGTGGTCCTTATGGTCCTTATGGTCCTTAGCGTCGCACCGAGGGAGATGGGCCTGGTCCTTAAGATGGGCCTGGTCCTTAAGATGGGCCCAGTTTCTAGATGGGCCGCGTCGCACAGTTTCTTGGTCCTTAAGATGGGCCTGGTCCTTAGCGTCGCACAGTTTCTAGATGGGCCCAGTTTCTCAGTTTCTCCGAGGGTGGTCCTTATGGTCCTTAAGATGGGCCCCGAGGGTGGTCCTTAGCGTCGCAGCGTCGCAGCGTCGCACAGTTTCTTGGTCCTTACAGTTTCTGCGTCGCA", 14)
-
-		println(result)
-	}
-
-	def dirtyFreqWords(text: String, k: Int):Map[String, Int] = {
+	def dirtyFreqWords(text: String, k: Int):(Int, List[String]) = {
 
 		var ms = scala.collection.mutable.Map[String,Int]()
 		@annotation.tailrec
 		def go(index: Int):Map[String, Int] = {
-			if(ms contains text.substring(index, index+k)) {
-				ms(text.substring(index, index+k)) += 1
+
+			text.substring(index,index+k) match {
+				case a if(ms contains a) => ms(a) +=1
+				case b => ms +=(b -> 1)
 			}
-			else ms +=(text.substring(index, index+k) -> 1)
 
 			if(index < (text.length - k)) go(index + 1) else ms
 
 		}
 		
-		go(0)
-
+		go(0).foldLeft((0, Nil: List[String]))((x: (Int, List[String]), y: (String, Int)) => y._2 match {
+			case a if (a > x._1) =>  (a, List(y._1))
+			case b if (b==x._1) => (b, y._1::x._2)
+			case _ => x
+		})
 
 	}
 }
