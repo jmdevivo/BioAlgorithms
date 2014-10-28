573722aee4035a45afebe1264bfaa7a28bb5431f
John Devivo <jdevivo@gmail.com>
2014-10-28 10:00:47 -0400

    PatternMatch now returns list of indexes in correct order

--- Week1/CodeChallanges/PatternMatch/PatternMatch.scala
+++ Week1/CodeChallanges/PatternMatch/PatternMatch.scala
@@ -6,15 +6,15 @@ object PatternMatch {
 
 	def patternMatch(text: String, pat: String):List[Int] = {
 		def go(l: List[Int], index: Int):List[Int] = {
-			if(index < (text.length - pat.length)) {
-				text.substring(index, index + pat.length) match {
-					case `pat` => go(index::l, index + 1)
-					case _ => go(l, index + 1)
+			if(index > pat.length) {
+				text.substring(index - pat.length, index) match {
+					case `pat` => go((index - pat.length)::l, index - 1)
+					case _ => go(l, index - 1)
 				}
 			}
 			else l
 		}
-		go(Nil, 0)
+		go(Nil, text.length)
 	}
 
 }
